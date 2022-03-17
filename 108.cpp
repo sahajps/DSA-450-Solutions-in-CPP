6f6b533a1e54c15dbf03b95a98281c0627d47b52
SAHAJPREET SINGH <97271680+sahajps@users.noreply.github.com>
2022-03-17 12:27:12 +0530

    Update 108.cpp
    O(N^2) was also excepted but this one is of O(N*log(N))

--- 108.cpp
+++ 108.cpp
@@ -2,24 +2,39 @@
 #include <algorithm>
 using namespace std;
 
+int binSrc(int arr[],int n,int x){
+	int i=0,j=n-1;
+	while(i<=j){
+		int mid=(i+j)/2;
+		if(arr[mid]<=x && (mid==n-1 || arr[mid+1]>x)){
+			return mid;
+		}
+		else if(arr[mid]>x){
+			j=mid-1;
+		}
+		else{
+			i=mid+1;
+		}
+	}
+	return -1;
+}
+
 int main(){
 	int n,r,St;
 	cin>>n;
-	int arr[n];
+	int arr[n],tot[n];
 	for(int i=0;i<n;i++) cin>>arr[i];
-	//sort(arr,arr+n);
-	cin>>r;
-	for(int i=0;i<r;i++){
-		cin>>St;
-		int cnt=0,tot=0;
-		for(int j=0;j<n;j++){
-			if(St>=arr[j]){
-				cnt++;
-				tot+=arr[j];
-			}
-		}
-		cout<<cnt<<" "<<tot<<endl;
 
+	sort(arr,arr+n);
+	tot[0]=arr[0];
+	for(int i=1;i<n;i++) tot[i]=tot[i-1]+arr[i];
+
+	cin>>r;
+	while(r--){
+		cin>>St;
+		int cnt=binSrc(arr,n,St);
+		cout<<cnt+1<<" "<<tot[cnt]<<endl;
 	}
 	return 0;
 }
+
